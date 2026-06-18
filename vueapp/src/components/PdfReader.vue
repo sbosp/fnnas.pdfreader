4ed6407db6cc1d3d740525d4dda2a32b39a2418e
asherqu <asherqu@tencent.com>
2026-06-18 12:59:33 +0800

    init first commit

--- vueapp/src/components/PdfReader.vue
+++ vueapp/src/components/PdfReader.vue
@@ -30,6 +30,19 @@ import PdfViewer from '@/components/PdfViewer.vue'
 import type {PdfPageData} from '@/composables/DataBean'
 import request from '@/utils/request'
 import {throttle, debounce} from '@/utils/UIUtils'
+
+if (typeof (Promise as any).withResolvers !== 'function') {
+  (Promise as any).withResolvers = function <T>() {
+    let resolve!: (value: T | PromiseLike<T>) => void
+    let reject!: (reason?: any) => void
+    const promise = new Promise<T>((res, rej) => {
+      resolve = res
+      reject = rej
+    })
+    return { promise, resolve, reject }
+  }
+}
+
 import * as pdfjsLib from 'pdfjs-dist'
 
 // Vite 原生标准写法，无需 ?url
